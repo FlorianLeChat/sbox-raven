be830391c4fba3cfe20e255514aedb93f7101ea9
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-07-28 22:02:26 +0200

    Add base and logs Raven methods

---
+++ code/custom/Base.cs
@@ -0,0 +1,69 @@
+﻿using Sandbox;
+
+partial class SandboxGame : Game
+{
+	/// <summary>
+	///
+	/// </summary>
+	[Event.Hotload]
+	public static void OnHotload()
+	{
+		Raven.Logs.Add( "Le serveur semble avoir procéder à un rechargement des fichiers." );
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	[Event( "OnClientJoined" )]
+	public static void OnClientJoined( Client client )
+	{
+/*		Positions = FileSystem.Data.ReadJsonOrDefault<Dictionary<ulong, int>>( "data/rts/ratings.json" ) ?? new();
+
+		Log.Info( Positions );*/
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	[Event( "OnPropSpawned" )]
+	public static void OnPropSpawned( string modelName )
+	{
+
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	[Event( "OnEntitySpawned" )]
+	public static void OnEntitySpawned( string entName )
+	{
+
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	[Event( "OnPlayerSpawned" )]
+	public static void OnPlayerSpawned()
+	{
+
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	[Event( "OnPlayerNoClip" )]
+	public static void OnPlayerNoClip( Client client )
+	{
+		// player.SendCommandToClient( "say \"Changement d'état du noclip\"" );
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	[Event( "OnPlayerTakeDamage" )]
+	public static void OnPlayerTakeDamage( DamageInfo info )
+	{
+
+	}
+}
---
+++ code/custom/Logs.cs
@@ -0,0 +1,39 @@
+﻿using Sandbox;
+
+namespace Raven
+{
+	public partial class Logs : Game
+	{
+		/// <summary>
+		///
+		/// </summary>
+		public enum Type
+		{
+			Warning = 0,
+			Error = 1,
+			Info = 2
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		[ClientRpc]
+		public static void SendChatMessage( string message )
+		{
+			_ = ConsoleSystem.Run( "chat_addinfo", message );
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		public static void Add( string message, bool toAdmins = true, int type = 0 )
+		{
+			if ( toAdmins )
+			{
+				SendChatMessage( message );
+			}
+
+			Log.Info( "[Raven] " + message );
+		}
+	}
+}
