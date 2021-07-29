fcb73cb2e6a47bc6b64f1e9300a5c860c9711ccc
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-07-29 21:51:02 +0200

    Add new class : `Characters`

---
+++ code/custom/Characters.cs
@@ -0,0 +1,23 @@
+using Sandbox;
+
+namespace Raven
+{
+	public partial class Characters : Game
+	{
+		/// <summary>
+		///
+		/// </summary>
+		public static void SetData( Client client, string key, string value = "" )
+		{
+
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		public static void GetData( Client client, string key, string value = "" )
+		{
+
+		}
+	}
+}
