32d30cd8f7ed4a5dda18539ee9582bc494d6f669
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-07-30 21:43:11 +0200

    Make logging system more robust

--- code/custom/Logs.cs
+++ code/custom/Logs.cs
@@ -7,9 +7,9 @@ namespace Raven
 	public partial class Logs : Game
 	{
 		/// <summary>
-		///
+		/// Énumérations pour les types de journalisation.
 		/// </summary>
-		public enum Type
+		public enum Type : byte
 		{
 			Warning = 0,
 			Error = 1,
@@ -17,29 +17,41 @@ namespace Raven
 		}
 
 		/// <summary>
-		///
+		/// Permet d'afficher un message dans le chat directement depuis un appel serveur.
 		/// </summary>
 		[ClientRpc]
 		public static void SendChatMessage( string message )
 		{
-			// https://stackoverflow.com/questions/8298614/php-regular-expression-failing
-			var matches = Regex.Matches( message, "[0-9]+");
+			var matches = Regex.Matches( message, "([0-9]{17,25})");
 			var steamID = matches.Count > 0 ? $"avatar:{matches[0]}" : null;
 
 			ChatBox.AddInformation( message, steamID );
 		}
 
 		/// <summary>
-		///
+		/// Permet de créer une nouvelle entrée dans la journalisation de la console.
 		/// </summary>
-		public static void Add( string message, bool toAdmins = true, int type = 0 )
+		public static void Add( string message, bool toAdmins = true, Type type = Type.Info )
 		{
-			if ( toAdmins )
-			{
-				SendChatMessage( message );
-			}
+			message = "[Raven] " + message;
 
-			Log.Info( "[Raven] " + message );
+			if ( toAdmins )
+				SendChatMessage( message );
+
+			switch ( type )
+			{
+				case Type.Warning:
+					Log.Warning( message );
+					break;
+
+				case Type.Error:
+					Log.Error( message );
+					break;
+
+				case Type.Info:
+					Log.Info( message );
+					break;
+			}
 		}
 	}
 }
